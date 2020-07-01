c72a8d864d78d9b3b1a3e952e7ded5df0ec994a7
Emely Garcia <garciam.emm@gmail.com>
2020-07-01 16:15:45 -0600

    agregando tarea a la lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { Tarea } from './tarea';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
@@ -9,16 +10,58 @@ import { Tarea } from './tarea';
 })
 export class AppComponent {
 	tareas: Tarea[];
+	nuevaTarea:FormGroup;
 
 	constructor(
-        public service: AppService,
+		public service: AppService,
+		public formBuilder:FormBuilder
 	) { }
 	
 	ngOnInit() {
 		this.obtenerTareas();
+		this.buildForm();
+	}
+
+	/**
+	 * Método que inicializa el formulario de nueva tarea
+	 */
+	buildForm() {
+		this.nuevaTarea = this.formBuilder.group({
+			titulo: ['',[Validators.required]],
+			minutos:[0,[Validators.required,Validators.min(0),Validators.pattern('[0-9]+')]] 
+		})
 	}
 
 	async obtenerTareas() {
 		this.tareas = await this.service.obtenerTareas();
+	}
+
+	/**
+	 * Método que agrega la tarea a la lista
+	 */
+	async crearTarea(){
+		if(this.nuevaTarea.valid){
+			console.log(this.nuevaTarea.value)
+			var values = this.nuevaTarea.value;
+			await this.service.crearTarea(values.titulo,Number(values.minutos))
+			.then(res=>{
+				alert("Tarea agregada con éxito");
+			})
+			.catch(err=>{
+				alert("Ha ocurrido un error al agregar la tarea");
+			});
+		}else{
+			//aviso de error
+			alert('La información no fue completada correctamente')
+		}
+	}
+
+	  /**
+	 * Función que hace la validacion de el atributo del formulario según las restricciones
+	 * retorna true si hay error
+	 * retorna false si no hay error
+	 */
+	handleError(controlName: string, errorName: string) {
+		return this.nuevaTarea.controls[controlName].hasError(errorName);
 	}
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,21 +3,41 @@ import { Tarea } from './tarea';
 
 @Injectable()
 export class AppService { 
-    constructor(
-        
-    ) { }
+    tareas: Tarea[] = [];
+    constructor() { 
+        this.llenarTareas();
+    }
 
     public async obtenerTareas() {
+        return this.tareas;
+    }
+
+    /**
+     * Método para llenar tareas
+     */
+    llenarTareas(){
         try {
-            var tareas: Tarea[] = [];
-            tareas.push(new Tarea(1, 'Sacar a pasear al perro', 15));
-            tareas.push(new Tarea(2, 'Sacar la basura', 5));
-            tareas.push(new Tarea(3, 'Cocinar la cena', 30));
-            tareas.push(new Tarea(4, 'Lavar la ropa', 50));
-            tareas.push(new Tarea(5, 'Regar las plantas', 20));
-            return tareas;
+            this.tareas.push(new Tarea(1, 'Sacar a pasear al perro', 15));
+            this.tareas.push(new Tarea(2, 'Sacar la basura', 5));
+            this.tareas.push(new Tarea(3, 'Cocinar la cena', 30));
+            this.tareas.push(new Tarea(4, 'Lavar la ropa', 50));
+            this.tareas.push(new Tarea(5, 'Regar las plantas', 20));
+            return this.tareas;
         } catch (error) {
             return null;
+        }
+    }
+
+    /**
+     * Método para agregar tarea a la lista
+     * @param titulo de la tarea
+     * @param minutos de la tarea
+     */
+    public async crearTarea(titulo:string,minutos:number){
+        try{
+            this.tareas.push(new Tarea(this.tareas.length,titulo,minutos));
+        }catch(error){
+            console.log("Error al agregar la tarea");
         }
     }
 }
